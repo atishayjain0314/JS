a3ddf8805e8dc2b30b3a91f3d3edc9708399591c
200110022atishay <atishayjain0314@gmail.com>
2022-11-19 02:24:30 +0530

    new

---
+++ object.js
@@ -0,0 +1,12 @@
+var student = {
+    "name" : "Atishay",
+    "Roll" : 200110022,
+    "subject" : meta,
+};
+
+var employee = {
+    "name" : "radha",
+    "income" : 0,
+};
+
+console.log(employee.Roll)
